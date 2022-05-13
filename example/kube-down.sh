72af7458df2ebc6aa528a713354de67009fa2d55
Alan Christie <alan.christie@matildapeak.com>
2022-05-13 22:40:31 +0100

    Style tweak

--- example/kube-down.sh
+++ example/kube-down.sh
@@ -5,9 +5,6 @@ if [ ! -d "roles" ]; then
     ln -s ../.. roles/matildapeak.garten
 fi
 
-ansible-playbook site.yml \
-    -e state=absent \
-    -e tfl_app_id=${TFL_APP_ID} \
-    -e tfl_app_key=${TFL_APP_KEY}
+ansible-playbook site.yml -e state=absent
 
 kubectl delete namespace/garten
