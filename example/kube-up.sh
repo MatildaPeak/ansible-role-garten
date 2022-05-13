6ae4b1adcbc40217ffb9af7c31534a31b3ad9ff9
Alan Christie <alan.christie@matildapeak.com>
2022-05-13 20:35:39 +0100

    Refactored for latest ARM64 build

--- example/kube-up.sh
+++ example/kube-up.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Run from a Python virtual environment...
+#
+#   python -m venv venv
+#   source venv/bin/activate
+#   pip install -r requirements.txt
+
 if [ ! -d "roles" ]; then
     mkdir roles
     ln -s ../.. roles/matildapeak.garten
@@ -8,6 +14,4 @@ fi
 kubectl create -f namespace.yml
 kubectl config set-context --current --namespace=garten
 
-ansible-playbook site.yml \
-    -e tfl_app_id=${TFL_APP_ID} \
-    -e tfl_app_key=${TFL_APP_KEY}
+ansible-playbook site.yaml
